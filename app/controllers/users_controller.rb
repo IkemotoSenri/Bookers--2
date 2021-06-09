b350f1c7ec893183d6897fb0ecefe6329112ec05
EC2 Default User <ec2-user@ip-172-31-1-165.ec2.internal>
2021-06-09 11:41:18 +0000

    [Update]booksの投稿閲覧削除編集完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,20 @@ class UsersController < ApplicationController
   @user = User.find(params[:id])
   @book = Book.new
  end
- 
+
+ def index
+  @user = User.all
+  @book = Book.all
+ end
+
  def edit
   @user = User.find(params[:id])
  end
-  
+
  def update
   @user = User.find(params[:id])
   @user.update(user_params)
   redirect_to user_path(@user.id)
  end
- 
+
 end
